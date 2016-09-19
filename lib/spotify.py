39b688dd933df20c5d857e440e6fef9ae1d9d0dc
chaseWilliams <bornofawesomeness@gmail.com>
2016-09-19 10:23:50 -0400

    planning

--- lib/spotify.py
+++ lib/spotify.py
@@ -9,24 +9,27 @@ import csv
 """ Spotify is a class that effectively abstracts the HTTP requests to the Spotify API
 
 An individual Spotify object that can act as a direct representation of a user's Spotify
+
 """
 
 class Spotify:
     api_base = 'https://api.spotify.com/v1'
-    api_library_tracks = api_base + '/me/tracks'
+    api_library_tracks = api_base + '/me/tracks' # only returns 50 songs at a time
     api_artists = api_base + '/artists'
+    api_track_metadata = api_base + '/audio-features' # note -> max of 100 ids
 
     def __init__(self, token):
         self.token = token
         self.songs = {}
         self.artists = {}
-        self._library()
+        self.song_metadata = np.ndarray([])
+        self._build_library()
 
-    # returns the count of all present genres in user's library
+    # returns a panda.DataFrame of all present genres in user's library
     def get_genres(self):
         return None
 
-    # returns the count of all the artists present in user's library
+    # returns a panda.DataFrame of all the artists present in user's library
     def get_artists(self):
         return None
 
@@ -38,7 +41,7 @@ class Spotify:
         df = pd.DataFrame(arr)
         return df
 
-    # returns all of the user's songs' attributes in an panda dataframe
+    # returns all of the user's songs' attributes in an panda.DataFrame
     def get_song_metadata(self):
         return None
 
@@ -50,12 +53,24 @@ class Spotify:
             for index, row in self.get_songs().iterrows():
                 writer.writerow(row.tolist())
 
+    # builds the playlists for the user to check out
+    def build_playlists(self):
+        return None
+
+    # returns a 7-song shortened version of the playlist
+    def playlist_sample(self, playlist):
+        return None
+
+    # saves the identified playlist to the user's Spotify
+    def save_playlist(self, playlist):
+        return None
+
     # handles all outgoing http requests
     def _get(self, endpoint):
         request = http.get(endpoint, headers={'Authorization': 'Bearer ' + self.token})
         return request
 
-    def _library(self):
+    def _build_library(self):
         # get the first track object to determine total number of tracks in library
         response = self._get(self.api_library_tracks + '?limit=1')
         response = response.json()
@@ -75,6 +90,12 @@ class Spotify:
         self.songs[track_object['name']] = track_object['id']
         for artist in track_object['artists']:
             self.artists[artist['name']] = artist['id']
+
+    # store all of the songs' metadata in a NumPy matrix, where index number is the same
+    # as Spotify.user_songs
+    def _metadata(self):
+        return None
+
 
 
 user = Spotify('BQB6iyAQUqZRoV9577SKQxLV6zRXhDnTS_mQauIu6xhDuegjLuEqV6Z4Qw10uEpboulyV8H06xAPZhiq9B6z-JtnA-Z_1odFQnXQBSLPcGTtkYh-89lo1EU_tTOzLlBopq5yUUjzv-bItMiLmwm-li4OZaBSSprr')
