c5108210c83751c5a10a30ad94544a3dfbb652e7
chaseWilliams <bornofawesomeness@gmail.com>
2017-02-22 20:12:58 -0500

    Adding functionality for additional playlist curation

---
+++ lib/curator.py
@@ -0,0 +1,27 @@
+from lib.timemachine import TimeMachine
+from lib.lastfm import Lastfm
+
+def make_filters():
+    pass
+
+def filter(df, feature, top_percentage, reverse=False):
+    max = df[feature].values.max()
+    if neglected:
+        temp_df = df.sort_values(feature, ascending=True)
+    else:
+        temp_df = df.sort_values(feature, ascending=False)
+    limit = 1 - int(len(temp_df) * top_percentage)
+    return temp_df.iloc[0:limit, :]
+
+def filter_in_range(df, feature, start, stop):
+    truth = (df[feature] >= start) & (df[feature] <= stop)
+    return df[truth]
+
+def tag_filter(df, genre):
+    truth_arr = [False] * len(df)
+    for index, genres in enumerate(df['genres']):
+        genres = genres.split(',')
+        if genre in genres:
+            truth_arr[index] = True
+    return df[truth_arr]
+
