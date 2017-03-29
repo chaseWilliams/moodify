4cdcfd0aca9377c7c7cdc3ff5d627cae59286639
Chase Williams <bornofawesomeness@gmail.com>
2017-03-29 13:53:06 +0000

    fixed async timout err

--- lib/batch.py
+++ lib/batch.py
@@ -25,7 +25,7 @@ def async_batch_requests(uris, response_func, callback, timeout, **kwargs):
             callback(**kwargs)
             
     for uri in uris:
-        req = HTTPRequest(uri, request_timeout=timeout)
+        req = HTTPRequest(uri, request_timeout=timeout, connect_timeout=timeout)
         count.inc()
         http_client.fetch(req, callback=response_wrapper)
     ioloop.IOLoop.instance().start()
